8294a1445646a705a9f1530cfed0f0af8afb3fd4
thecodegator <thecodegator@gmail.com>
2016-02-25 18:21:29 +0530

    Added calculator

---
+++ calc.py
@@ -0,0 +1,13 @@
+a = input("Enter a: ")
+b = input("Enter b: ")
+
+print(" 1. Add \n 2. Sub \n 3. Div \n 4. Mul")
+a = input()
+if a == 1:
+    print a + b
+elif a == 2:
+    print a - b
+elif a == 3:
+    print a / b
+else:
+    print a * b
